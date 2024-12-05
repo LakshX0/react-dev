a1d1ff04054ac8d65baf32f87ecc8949c42d5278
LakshX0 <lakshansandeep7@gmail.com>
2024-12-06 04:14:11 +0530

    updated componets features

---
+++ react-18-mosh/src/Alert.tsx
@@ -0,0 +1,25 @@
+// let alertText = "Your item successfully saved!";
+
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  onClose: () => void;
+}
+
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className={"alert alert-success alert-dismissible"}>
+      {children}
+      <button
+        onClick={onClose}
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
+};
+
+export default Alert;
--- react-18-mosh/src/App.tsx
+++ react-18-mosh/src/App.tsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
+import Alert from "./Alert";
 import ListGroup from "./assets/components/ListGroup";
+import Button from "./Button";
 
 function App() {
+  const [alertVisible, setAlertVisible] = useState(false);
+
+  const cities = [
+    "Ratnapura",
+    "Colombo",
+    "Kandy",
+    "Trincomalee",
+    "Nuwara Eliya",
+    "Gampaha",
+  ];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={cities}
+        heading="Cities of Sri Lanka"
+        onSelectItem={handleSelectItem}
+      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>
+          Your activity is saved!
+        </Alert>
+      )}
+      <Button color="danger" onClick={() => setAlertVisible(true)}>
+        Lakshan
+      </Button>
     </div>
   );
 }
